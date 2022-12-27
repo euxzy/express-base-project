05312fde21949811e79d07917b3ae0fb77f2be65
euxzy <rijal.m12l@gmail.com>
2022-12-27 08:43:26 +0700

    [ + ] add env config

--- app/models/index.js
+++ app/models/index.js
@@ -6,25 +6,13 @@ const Sequelize = require('sequelize')
 const process = require('process')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(__dirname + '/../config/config.js')[env]
 const User = require('./user')
 const db = {}
 
 let sequelize
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    operatorsAliases: false,
-
-    pool: {
-      max: config.pool.max,
-      min: config.pool.min,
-      acquire: config.pool.acquire,
-      idle: config.pool.idle,
-    },
-  })
-  // sequelize = new Sequelize(process.env[config.use_env_variable], config)
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
   sequelize = new Sequelize(
     config.database,
